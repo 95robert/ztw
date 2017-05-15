23902c243143b495180cc1409e766b2bbecd7747
Aksel Nooitgedagt <akselon@gmail.com>
2017-05-15 10:33:30 +0200

    Back button

--- web/src/app/components/app.component.js
+++ web/src/app/components/app.component.js
@@ -5,15 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 /**
  * Created by akselon on 2017-04-24.
  */
 var core_1 = require("@angular/core");
+var common_1 = require("@angular/common");
+var router_1 = require("@angular/router");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(location, router) {
+        var _this = this;
+        this.location = location;
         this.title = 'Bettinger';
         this.locale = document['locale'];
         this.menuShowed = false;
+        this.showBackLink = false;
+        this.firstUrl = '';
+        this.sub = router.events
+            .filter(function (event) { return event instanceof router_1.NavigationStart; })
+            .subscribe(function (e) {
+            var url = e.url;
+            if (_this.firstUrl === '') {
+                _this.firstUrl = url;
+            }
+            else if (_this.firstUrl !== url) {
+                _this.showBackLink = true;
+                _this.sub.unsubscribe();
+            }
+        });
     }
     AppComponent.prototype.toggleMenu = function () {
         this.menuShowed = !this.menuShowed;
@@ -23,6 +44,9 @@ var AppComponent = (function () {
     AppComponent.prototype.menuLinkClicked = function () {
         this.menuShowed = false;
     };
+    AppComponent.prototype.menuLinkBack = function () {
+        this.location.back();
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
@@ -30,7 +54,9 @@ AppComponent = __decorate([
         selector: 'my-app',
         templateUrl: './assets/app.component.html',
         styleUrls: ['./assets/app.component.css']
-    })
+    }),
+    __metadata("design:paramtypes", [common_1.Location,
+        router_1.Router])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- web/src/app/components/app.component.ts
+++ web/src/app/components/app.component.ts
@@ -2,6 +2,8 @@
  * Created by akselon on 2017-04-24.
  */
 import { Component } from '@angular/core';
+import {Location} from '@angular/common';
+import {NavigationStart, Router} from '@angular/router';
 @Component({
     selector: 'my-app',
     templateUrl: './assets/app.component.html',
@@ -11,6 +13,23 @@ export class AppComponent {
     title = 'Bettinger';
     locale = document['locale'] as string;
     menuShowed = false;
+    showBackLink = false;
+    firstUrl = '';
+    private sub: any;
+    constructor(private location: Location,
+                router: Router) {
+        this.sub = router.events
+            .filter(event => event instanceof NavigationStart)
+            .subscribe(e => {
+                let url = e.url;
+                if (this.firstUrl === '') {
+                    this.firstUrl = url;
+                } else if (this.firstUrl !== url) {
+                    this.showBackLink = true;
+                    this.sub.unsubscribe();
+                }
+            });
+    }
     public toggleMenu() {
         this.menuShowed = !this.menuShowed;
         console.log(this.locale);
@@ -18,6 +37,9 @@ export class AppComponent {
     }
     public menuLinkClicked () {
         this.menuShowed = false;
+    }
+    public menuLinkBack() {
+        this.location.back();
     }
 }
 
